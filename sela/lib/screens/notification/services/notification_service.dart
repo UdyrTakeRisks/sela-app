f0014e71565769591dc109fd1af5e96f765597f0
OmarZen <omarwaleedzenhom2002@gmail.com>
2024-07-05 00:30:57 +0300

    Notification Functionality fix

--- sela/lib/screens/notification/services/notification_service.dart
+++ sela/lib/screens/notification/services/notification_service.dart
@@ -57,80 +57,76 @@ class NotificationService {
     print(cookie);
 
     try {
-      List<String> notifications = [];
-      List<String> NotificationToDatabase = [];
-      while (true) {
-        // Fetch new notifications from the queue
-        final response = await http.get(
-          Uri.parse(url),
-          headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Cookie': cookie ?? '', // Include the cookie if available
-          },
-        );
+      String notification;
+      String notificationAddToDatabase;
 
-        print(response.body);
-        if (response.statusCode == 200) {
-          // If the response is a plain text message, add it to the list
-          notifications.add(response.body);
-        } else if (response.statusCode == 400) {
-          // Stop fetching when the queue returns a 400 response code
-          break;
-        } else if (response.statusCode == 401) {
-          throw Exception('Unauthorized');
-        } else if (response.statusCode == 403) {
-          throw Exception('Forbidden');
-        } else if (response.statusCode == 404) {
-          print('No new notifications found');
-          break;
-        } else {
-          throw Exception('Failed to load new notifications');
-        }
+      // Fetch new notifications from the queue
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Cookie': cookie ?? '', // Include the cookie if available
+        },
+      );
+
+      print(response.body);
+      if (response.statusCode == 200) {
+        // If the response is a plain text message, add it to the list
+        notification = response.body;
+      } else if (response.statusCode == 400) {
+        throw Exception('Bad request');
+      } else if (response.statusCode == 401) {
+        throw Exception('Unauthorized');
+      } else if (response.statusCode == 403) {
+        throw Exception('Forbidden');
+      } else if (response.statusCode == 404) {
+        return [response.body];
+      } else {
+        throw Exception('Failed to load new notifications');
       }
-      NotificationToDatabase = await addNotification(notifications);
-      print('New notifications: $notifications');
-      return notifications;
+
+      notificationAddToDatabase = await addNotification(notification);
+      print('New notifications: $notification');
+      return [notification];
     } catch (e) {
       print('Error fetching new notifications: $e');
       throw Exception('Failed to load new notifications');
     }
   }
 
-  static Future<List<String>> addNotification(List<String> message) async {
+  static Future<String> addNotification(String message) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     final String? cookie = prefs.getString('cookie');
 
     // Replace with actual API URL
     const String url = '$DOTNET_URL_API_BACKEND/Notification/add';
 
-    List<String> notificationsAddList = [];
     try {
-      for (String msg in message) {
-        final response = await http.post(
-          Uri.parse(url),
-          headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Cookie': cookie ?? '', // Include the cookie if available
-          },
-          body: jsonEncode(<String, String>{
-            'message': msg,
-          }),
-        );
-
-        if (response.statusCode == 200) {
-          notificationsAddList.add(response.body);
-        } else if (response.statusCode == 401) {
-          throw Exception('Unauthorized');
-        } else if (response.statusCode == 403) {
-          throw Exception('Forbidden');
-        } else if (response.statusCode == 404) {
-          throw Exception('Notification not found');
-        } else {
-          throw Exception('Failed to add notification');
-        }
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Cookie': cookie ?? '', // Include the cookie if available
+        },
+        body: jsonEncode(<String, String>{
+          'message': message,
+        }),
+      );
+      print(response.body);
+      if (response.statusCode == 200) {
+        print('Notification added successfully');
+        return response.body;
+      } else if (response.statusCode == 400) {
+        throw Exception('Bad request');
+      } else if (response.statusCode == 401) {
+        throw Exception('Unauthorized');
+      } else if (response.statusCode == 403) {
+        throw Exception('Forbidden');
+      } else if (response.statusCode == 404) {
+        throw Exception('Notification not found');
+      } else {
+        throw Exception('Failed to add notification');
       }
-      print('Added notifications: $notificationsAddList');
-      return notificationsAddList;
     } catch (e) {
       print('Error adding notification: $e');
       throw Exception('Failed to add notification');
